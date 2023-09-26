b777568b5deb767a9f81a49d3314111a07433d8c
Sakina Mbatha <sakina@natix.io>
2023-09-26 11:16:12 +0200

    feat: navigation and state
    convert to stateful widget to get access
    to the context object, enable user to access
    the form from the app bar

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Groceries'),
-      ),
+      appBar: AppBar(title: const Text('Your Groceries'), actions: [
+        IconButton(
+          onPressed: _addItem,
+          icon: const Icon(Icons.add),
+        )
+      ]),
       body: ListView.builder(
           itemCount: groceryItems.length, // necessary for optimization
           itemBuilder: (context, index) {
