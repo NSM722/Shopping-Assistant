a00ad17630c525f61aeb841c1fbd68bbce1dd020
Sakina Mbatha <sakina@natix.io>
2023-09-27 14:16:19 +0200

    feat: fetch and transform data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // gives access to json methods
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,25 +14,62 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // load the items when the app initializes
+    _loadItems();
+  }
+
+  // fetch data from BE
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-http-requests-ce06f-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    final response = await http.get(url);
+
+    // output of data received from the BE to understand the way it has been typed
+    /// {"-NfL7T2bIn80QIPeNsq_":{"category":"Hygiene","name":"Tampons","quantity":2},"-NfLG1XahanrxthIdg1P":{"category":"Other","name":"Tanqueray","quantity":3}}
+    final Map<String, dynamic> listData =
+        json.decode(response.body); // convert json to Map
+
+    // format to list
+    // the output of listData.entries
+    /// [MapEntry("-NfL7T2bIn80QIPeNsq_", {category: Hygiene, name: Tampons, quantity: 2}), MapEntry("-NfLG1XahanrxthIdg1P", {category: Other, name: Tanqueray, quantity: 3})]
+
+    /// convert the map entries to a list of grocery items
+    /// this is a temporary list to replace _groceryItems
+    final List<GroceryItem> _loadedItems = [];
+
+    for (final item in listData.entries) {
+      // get the first matching item
+      final category = categories.entries
+          .firstWhere(
+              (element) => element.value.title == item.value['category'])
+          .value;
+
+      _loadedItems.add(GroceryItem(
+        id: item.key, // the automatically generated key in Firebase
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+
+    // re-assign the _groceryItems
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    // receive data from the new_item screen asynchronously
-    final newItem =
-        await Navigator.of(context).push<GroceryItem>(MaterialPageRoute(
+    Navigator.of(context).push<GroceryItem>(MaterialPageRoute(
       builder: (context) => const NewItem(),
     ));
 
-    // new item could be null incase the user presses the back button
-    // hence handling such use-case
-    if (newItem == null) {
-      return; // return nothing
-    }
-
-    // else add it to the list
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
